946605f24a94acbb5a713d78baa7af87d27fbe17
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-04-16 22:44:16 +0300

    Create Road repository

---
+++ src/main/java/com/zelenev/repositories/RoadRepository.java
@@ -0,0 +1,7 @@
+package com.zelenev.repositories;
+
+import com.zelenev.data.entities.Road;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoadRepository extends CrudRepository<Road, Integer> {
+}
