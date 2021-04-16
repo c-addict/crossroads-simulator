f0699136f553ff7ab1790c02d475e3254b764e7d
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-04-16 23:17:26 +0300

    Update Traffic light service

--- src/main/java/com/zelenev/services/implementations/TrafficLightService.java
+++ src/main/java/com/zelenev/services/implementations/TrafficLightService.java
@@ -1,7 +1,36 @@
-package com.zelenev.services;
+package com.zelenev.services.implementations;
 
+import com.zelenev.data.dto.TrafficLightDto;
+import com.zelenev.services.ICrudService;
 import org.springframework.stereotype.Service;
 
-@Service
-public class TrafficLightService {
+import java.util.List;
+
+@Service("trafficLightService")
+public class TrafficLightService implements ICrudService<TrafficLightDto> {
+
+    @Override
+    public TrafficLightDto read(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<TrafficLightDto> readAll() {
+        return null;
+    }
+
+    @Override
+    public void create(TrafficLightDto trafficLightDto) {
+
+    }
+
+    @Override
+    public void update(TrafficLightDto trafficLightDto, TrafficLightDto newT) {
+
+    }
+
+    @Override
+    public void delete(TrafficLightDto trafficLightDto) {
+
+    }
 }
