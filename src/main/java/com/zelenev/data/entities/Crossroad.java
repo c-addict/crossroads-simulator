c74784a0c2ff2947300b1006f0c7694a3fe69c3b
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-04-16 22:43:25 +0300

    Create Crossroad entitie

--- src/main/java/com/zelenev/data/entities/Crossroad.java
+++ src/main/java/com/zelenev/data/entities/Crossroad.java
@@ -1,4 +1,28 @@
-package com.zelenev.model.data.entities;
+package com.zelenev.data.entities;
 
-public class Crossroad {
+import com.zelenev.data.physics.TwoDimensionsPhysicalObject;
+
+public class Crossroad extends TwoDimensionsPhysicalObject {
+
+    private Integer id;
+
+    private Road mainRoad;
+
+    private Road crossRoad;
+
+    public Road getMainRoad() {
+        return mainRoad;
+    }
+
+    public void setMainRoad(Road mainRoad) {
+        this.mainRoad = mainRoad;
+    }
+
+    public Road getCrossRoad() {
+        return crossRoad;
+    }
+
+    public void setCrossRoad(Road crossRoad) {
+        this.crossRoad = crossRoad;
+    }
 }
