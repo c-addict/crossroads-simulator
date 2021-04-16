5f8ae0eba67b13191ee024d639632e691bffc3b7
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-04-16 22:55:42 +0300

    Create Crossroad to Crossroad dto converter

---
+++ src/main/java/com/zelenev/converters/implementations/CrossroadToCrossroadDtoConverter.java
@@ -0,0 +1,26 @@
+package com.zelenev.converters.implementations;
+
+import com.zelenev.converters.BaseObjectConverter;
+import com.zelenev.data.dto.CrossroadDto;
+import com.zelenev.data.entities.Crossroad;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CrossroadToCrossroadDtoConverter extends BaseObjectConverter<Crossroad, CrossroadDto> {
+
+    @Override
+    public CrossroadDto convert(Crossroad obj) {
+        //TODO: complete crossroad to crossroad dto converter
+        return null;
+    }
+
+    @Override
+    public Class<Crossroad> getInClass() {
+        return Crossroad.class;
+    }
+
+    @Override
+    public Class<CrossroadDto> getOutClass() {
+        return CrossroadDto.class;
+    }
+}
