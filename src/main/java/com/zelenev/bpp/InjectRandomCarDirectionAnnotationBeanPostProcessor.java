f5d5cc462e9d3f544ef0ab0e31b6c4fdae2ecb77
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-04-16 23:04:22 +0300

    Change Random.java to Apache Commons RandomUtils

--- src/main/java/com/zelenev/bpp/InjectRandomCarDirectionAnnotationBeanPostProcessor.java
+++ src/main/java/com/zelenev/bpp/InjectRandomCarDirectionAnnotationBeanPostProcessor.java
@@ -1,23 +1,19 @@
 package com.zelenev.bpp;
 
 import com.zelenev.annotations.InjectRandomCarDirection;
-import com.zelenev.model.data.states.CarDirection;
+import com.zelenev.data.states.CarDirection;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.util.ReflectionUtils;
-
 import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Random;
-
 public class InjectRandomCarDirectionAnnotationBeanPostProcessor implements BeanPostProcessor {
 
     @Autowired
     private List<CarDirection> directions;
 
-    @Autowired
-    private Random random;
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -25,7 +21,7 @@ public class InjectRandomCarDirectionAnnotationBeanPostProcessor implements Bean
         for (Field field : fields) {
             InjectRandomCarDirection fieldAnnotation = field.getAnnotation(InjectRandomCarDirection.class);
             if (fieldAnnotation != null) {
-                int randomDirectionIndex = random.nextInt(directions.size());
+                int randomDirectionIndex = RandomUtils.nextInt(0, directions.size());
                 CarDirection randomDirection = directions.get(randomDirectionIndex);
                 field.setAccessible(true);
                 ReflectionUtils.setField(field, bean, randomDirection);
