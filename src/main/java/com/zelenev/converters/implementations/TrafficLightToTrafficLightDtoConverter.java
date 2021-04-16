8411443e1ae8d99ea62893b2015841bd05697783
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-04-16 22:56:12 +0300

    Create Traffic light to Traffic light dto converter

---
+++ src/main/java/com/zelenev/converters/implementations/TrafficLightToTrafficLightDtoConverter.java
@@ -0,0 +1,26 @@
+package com.zelenev.converters.implementations;
+
+import com.zelenev.converters.BaseObjectConverter;
+import com.zelenev.data.dto.TrafficLightDto;
+import com.zelenev.data.entities.TrafficLight;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrafficLightToTrafficLightDtoConverter extends BaseObjectConverter<TrafficLight, TrafficLightDto> {
+
+    @Override
+    public TrafficLightDto convert(TrafficLight obj) {
+        //TODO: complete traffic light to traffic light dto converter
+        return null;
+    }
+
+    @Override
+    public Class<TrafficLight> getInClass() {
+        return null;
+    }
+
+    @Override
+    public Class<TrafficLightDto> getOutClass() {
+        return null;
+    }
+}
